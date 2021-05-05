b04424da435ce79d48b6bf7a12c2045d50584b3b
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-05 03:31:31 -0400

    Creando el servidor de node

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// Cargar módulos de node para crear el servidor
+let express = require('express');
+let bodyParser = require('body-parser');
+
+// Ejecutar express (http)
+let app = express();
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// CORS
+
+// Añadir prefijos a rutas
+
+
+// Ruta o método de prueba
+app.get('/probando', (req,res) =>{
+    
+    return res.status(200).send({
+        area: 'Practicando nodeJS',
+        autor: 'Leonardo Guilarte',
+        url: 'leomiguel.com'
+    })
+})
+
+// Exportar modulo (fichero actual)
+module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 let mongoose = require('mongoose');
+let app = require('./app');
+let port = 3900;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -8,7 +10,13 @@ mongoose.connect('mongodb://localhost/api_rest_blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log('La conexión a la base de datos correcta!!!')
+    console.log('La conexión a la base de datos correcta!!!');
+
+    // Crear servidor y ponerme a escuchar peticiones HTTP
+    app.listen(port, () => {
+        console.log('Servidor corriendo em http://localhost:'+port)
+    })
+
 }).catch(err=>{
     console.log(`db error ${err.message}`);
     process.exit(-1)
