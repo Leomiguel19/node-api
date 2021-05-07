c7878dde0db76ee977a35a335ab1ffea2e323d49
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-06 22:47:08 -0400

    Acción para guardar artículos y validación de datos

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,8 @@
 'use strict'
 
+let validator = require('validator');
+var Article = require('../models/article');
+
 let controller = {
     
     datosCurso: (req, res) => {
@@ -17,6 +20,41 @@ let controller = {
         return res.status(200).send({
             message: 'Soy la acción test del controlador de articulos'
         })
+    },
+    
+    save: (req, res) => {
+        // Recoger parámetros por post
+        let params = req.body; 
+
+        // Validar datos (validator)
+        try{
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_description = !validator.isEmpty(params.description);
+        }catch(err){
+            return res.status(200).send({
+                message: 'Faltan datos por enviar!!'
+            })
+        }
+
+        if(validate_title && validate_description){            
+            // Crear el objeto a guardar
+
+            // Asignar valores
+
+            // Guardar el artículo
+
+            // Devolver una respuesta
+
+            return res.status(200).send({
+                article: params
+            })
+        }else{
+            return res.status(200).send({
+                message: 'Los datos no son válidos!!'
+            })
+        }
+
+
     }
 };
 
--- routes/article.js
+++ routes/article.js
@@ -5,7 +5,11 @@ let ArticleController = require('../controllers/article');
 
 let router = express.Router();
 
+// Rutas de prueba
 router.post('/datos-curso', ArticleController.datosCurso);
 router.get('/test', ArticleController.test);
+
+// Rutas útiles
+router.post('/save', ArticleController.save);
 
 module.exports = router;
